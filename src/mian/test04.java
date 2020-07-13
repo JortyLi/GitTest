b1ae1ac6bba0cf81a80fd0594566009737133475
heng.li_1@medbanks.cn <VBte4baeL>
2020-07-13 14:34:20 +0800

    整合测试

---
+++ src/mian/test04.java
@@ -0,0 +1,12 @@
+package mian;
+
+/**
+ * @author liheng
+ * @data 2020-07-13 14:33
+ */
+public class test04 {
+
+    public static void main(String[] args) {
+        System.out.println("整合测试");
+    }
+}
